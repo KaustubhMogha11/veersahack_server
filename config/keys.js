2ae17d378fc93750bde95ab13f7f5afbf0bb4270
KaustubhMogha11 <70569075+KaustubhMogha11@users.noreply.github.com>
2022-09-06 16:44:20 +0530

    added config

---
+++ config/keys.js
@@ -0,0 +1,25 @@
+module.exports = {
+  mongodb: {
+    mongoURI:"mongodb+srv://nik:kuchbhi@cluster0.md8ne.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  },
+  ibm_key: {
+    SPEECH_TO_TEXT_URL:"https://api.eu-gb.speech-to-text.watson.cloud.ibm.com/instances/d5c93676-e0f3-45ec-9fab-1a87e447fb43",
+    SPEECH_TO_TEXT_IAM_APIKEY: "ljm38xrN3SDdzs3gDufiIcB0in7P17f0mTeMGq_MJlOe",
+  },
+  agora: {
+    APP_ID: "370cc8b63bac46d381f17915984b033d",
+    APP_CERTIFICATE: "3b45c4e6524742c2ae81736ffbc6f673",
+  },
+  typeform:{
+    token:"tfp_HBXuivyghDCLADcjJAZZY6qhMVNy4z7UG6gDZ9CnftF2_3spr71h8ZFULS7"
+  },
+  firebase:{
+    apiKey: "AIzaSyCCUIaIb5k9T0lP1IfZOsRo-mGCn57_ToQ",
+    authDomain: "shrink4shrink.firebaseapp.com",
+    projectId: "shrink4shrink",
+    storageBucket: "shrink4shrink.appspot.com",
+    messagingSenderId: "299205315787",
+    appId: "1:299205315787:web:d9223453b7c56cc8e75f4e",
+    measurementId: "G-946JJW6SDB"
+  }
+};
